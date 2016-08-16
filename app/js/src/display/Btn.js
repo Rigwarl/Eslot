c291a7fe30edfce6605edf1cd8f0cd1477ad9b24
Rigwarl <Avips@mail.ru>
2016-08-16 17:14:28 +0300

    add disable method to btns

--- app/js/src/display/Btn.js
+++ app/js/src/display/Btn.js
@@ -2,6 +2,8 @@ export default class Btn extends createjs.Container {
   constructor(text) {
     super();
 
+    this.disabled = false;
+
     this.createBg();
     this.createText(text);
     this.bindEvents();
@@ -17,12 +19,26 @@ export default class Btn extends createjs.Container {
     this.text.y = 8;
     this.addChild(this.text);
   }
+  enable() {
+    this.disabled = false;
+    this.dispatchEvent('rollout');
+  }
+  disable() {
+    this.disabled = true;
+    this.bg.graphics = new createjs.Graphics().beginFill('#666666').drawRoundRect(0, 0, 150, 50, 25);
+  }
   bindEvents() {
     this.addEventListener('rollover', () => {
+      if (this.disabled) {
+        return;
+      }
       this.bg.graphics = new createjs.Graphics().beginFill('#ff0000').drawRoundRect(0, 0, 150, 50, 25);
       this.cursor = 'pointer';
     });
     this.addEventListener('rollout', () => {
+      if (this.disabled) {
+        return;
+      }
       this.bg.graphics = new createjs.Graphics().beginFill('#00ff00').drawRoundRect(0, 0, 150, 50, 25);
       this.cursor = null;
     });
