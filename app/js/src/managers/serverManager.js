9010ece08ef4b77e5021054fead1cefee64b0171
Rigwarl <Avips@mail.ru>
2016-08-22 12:25:24 +0300

    now server always return data obj with all fields

--- app/js/src/managers/serverManager.js
+++ app/js/src/managers/serverManager.js
@@ -1,23 +1,34 @@
+const data = {
+  points: 5300,
+  win: 0,
+  finished: false,
+  bet: 3,
+  minBet: 2,
+  maxBet: 5,
+  maxPoints: 7000,
+  winTable: [1, 2, 5, 25, 50],
+  symbols: null,
+};
+
 const serverManager = {
-  currentPoints: 1300,
   getData() {
-    return new Promise(resolve => setTimeout(() => resolve({
-      bet: 3,
-      minBet: 2,
-      maxBet: 5,
-      points: this.currentPoints,
-      maxPoints: 7000,
-      winTable: [1, 2, 5, 25, 50],
-    }), 1500));
+    return new Promise(resolve => setTimeout(() => resolve(data), 1500));
   },
   roll(bet) {
     return new Promise(resolve => setTimeout(() => {
-      this.currentPoints += 350 * bet;
-      resolve({
-        win: 350 * bet,
-        points: this.currentPoints,
-        symbols: ['Л', 'О', 'Л'],
-      });
+      data.bet = bet;
+      data.win = bet * 350;
+      data.symbols = ['Л', 'О', 'Л'];
+
+      const newPoints = data.points + data.win;
+      if (newPoints >= data.maxPoints) {
+        data.points = data.maxPoints;
+        data.finished = true;
+      } else {
+        data.points = newPoints;
+      }
+
+      resolve(data);
     }, 1500));
   },
 };
